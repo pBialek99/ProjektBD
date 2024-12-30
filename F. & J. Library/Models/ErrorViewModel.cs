81aa1fe7bd135bc36aee2ef8b2f4d8824b36da69
Paweł Białek <pawel99bialek@gmail.com>
2024-12-30 18:58:38 +0100

    Add project files.

---
+++ F. & J. Library/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace F.___J._Library.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
